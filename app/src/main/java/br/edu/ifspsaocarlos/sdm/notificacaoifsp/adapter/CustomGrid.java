170d592669599c2edf38cfac290a35f1cfb544af
raphaelgava <raphaelgava1@gmail.com>
2017-06-29 02:36:09 -0300

    New grid

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/notificacaoifsp/adapter/CustomGrid.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/notificacaoifsp/adapter/CustomGrid.java
@@ -1,14 +1,29 @@
 package br.edu.ifspsaocarlos.sdm.notificacaoifsp.adapter;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -18,13 +33,54 @@ import br.edu.ifspsaocarlos.sdm.notificacaoifsp.R;
 import br.edu.ifspsaocarlos.sdm.notificacaoifsp.layout.GridNotificationsFragment;
 import br.edu.ifspsaocarlos.sdm.notificacaoifsp.model.Oferecimento;
 
+/*
+public class CustomGrid extends ArrayAdapter<Oferecimento> {
+    private LayoutInflater inflador;
+
+    public CustomGrid(Activity tela, List<Oferecimento> listaOferecimentos) {
+        super(tela, R.layout.celula_grid, listaOferecimentos);
+        inflador = (LayoutInflater) tela.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if ( convertView == null ) {
+// infla uma nova célula
+            convertView = inflador.inflate(R.layout.celula_grid, null);
+            holder = new ViewHolder();
+
+            holder.txtSigla = (TextView) convertView.findViewById(R.id.tv_sigla);
+            holder.txtDate = (TextView) convertView.findViewById(R.id.tv_data);
+            //mLayoutPrincipal = (LinearLayout) itemView.findViewById(R.id.cell_layout2);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Oferecimento offer = getItem(position);
+        holder.txtSigla.setText(offer.getSigla());
+        holder.txtDate.setText(offer.getDataString());
+        return convertView;
+    }
+    static class ViewHolder {
+        private TextView txtSigla;
+        private TextView txtDate;
+    }
+}
+*/
+
 public class CustomGrid extends RecyclerView.Adapter<CustomGrid.ItemViewHolder> {
 
     private List<Oferecimento> mListaOferecimentos;
+    private Point windowSize;
+    private int position;
+    private final double INDEX_COLUMN = 0.20;
 
 
-    public CustomGrid(List<Oferecimento> listaOferecimentos) {
+    public CustomGrid(List<Oferecimento> listaOferecimentos, Point size, int position) {
         this.mListaOferecimentos = listaOferecimentos;
+        this.windowSize = size;
+        this.position = position;
     }
 
     @Override
@@ -44,18 +100,35 @@ public class CustomGrid extends RecyclerView.Adapter<CustomGrid.ItemViewHolder> 
         holder.txtDate.setText(offer.getDataString());
         holder.mLayoutPrincipal.setTag(position);
 
-        Log.d("TCC", "index" + position);
-        if ((position % GridNotificationsFragment.NUMBER_COLUMN == 0) ||
-                (position % GridNotificationsFragment.NUMBER_COLUMN == 2) ||
-                (position % GridNotificationsFragment.NUMBER_COLUMN == 4)){
-            holder.mLayoutPrincipal.setBackgroundResource(R.color.colorCell);
-            //holder.mLayoutPrincipal.setBackgroundColor(0xAA32CD32);
-            Log.d("TCC", "Green" + position);
-        }
-        else{
-            holder.mLayoutPrincipal.setBackgroundColor(Color.WHITE);
-            Log.d("TCC", "White" + position);
-        }
+        holder.mLayoutPrincipal.setBackgroundResource(R.drawable.shape_color);
+
+//        Drawable shape;
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            shape = holder.mLayoutPrincipal.getResources().getDrawable(R.drawable.shape_color, null);
+//        }
+//        else
+//        {
+//            shape = holder.mLayoutPrincipal.getResources().getDrawable(R.drawable.shape_color);
+//        }
+//
+//        Log.d("TCC", "index" + position);
+//        if ((position % GridNotificationsFragment.NUMBER_COLUMN == 0) ||
+//                (position % GridNotificationsFragment.NUMBER_COLUMN == 2) ||
+//                (position % GridNotificationsFragment.NUMBER_COLUMN == 4)){
+//            //holder.mLayoutPrincipal.setBackgroundResource(R.color.colorCell);
+//            holder.mLayoutPrincipal.setBackgroundResource(R.drawable.shape_color);
+//            //shape.setColorFilter(holder.mLayoutPrincipal.getResources().getColor(R.color.colorCell), PorterDuff.Mode.SRC_IN);
+//            //holder.mLayoutPrincipal.setBackgroundColor(0xAA32CD32);
+//            Log.d("TCC", "Green" + position);
+//        }
+//        else{
+//            //holder.mLayoutPrincipal.setBackgroundColor(Color.WHITE);
+//            //holder.mLayoutPrincipal.setBackgroundResource(R.drawable.shape_border);
+//            shape.setColorFilter(holder.mLayoutPrincipal.getResources().getColor(R.color.colorOptionMap), PorterDuff.Mode.SRC_IN);
+//            Log.d("TCC", "White" + position);
+//        }
+//        holder.mLayoutPrincipal.setBackground(shape);
     }
 
     @Override
@@ -68,6 +141,8 @@ public class CustomGrid extends RecyclerView.Adapter<CustomGrid.ItemViewHolder> 
         private LinearLayout mLayoutPrincipal;
         private TextView txtSigla;
         private TextView txtDate;
+        //private CardView card;
+        private LinearLayout cell;
 
         public ItemViewHolder(final View itemView) {
             super(itemView);
@@ -75,6 +150,25 @@ public class CustomGrid extends RecyclerView.Adapter<CustomGrid.ItemViewHolder> 
             txtSigla = (TextView) itemView.findViewById(R.id.tv_sigla);
             txtDate = (TextView) itemView.findViewById(R.id.tv_data);
             mLayoutPrincipal = (LinearLayout) itemView.findViewById(R.id.cell_layout2);
+
+            /* //PEGAR ALTURA DA VIEW
+            TypedValue tv = new TypedValue();
+            int actionBarHeight = 0;
+
+            if (itemView.getContext().getTheme().resolveAttribute(R.attr.actionBarSize, tv, true))
+            {
+                actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,itemView.getResources().getDisplayMetrics());
+            }
+            */
+
+            int height;
+            int width;
+
+            height = (int) ((windowSize.y - position) * INDEX_COLUMN);
+            width = (int) (windowSize.x * INDEX_COLUMN);
+
+            cell = (LinearLayout) itemView.findViewById(R.id.cell_layout2);
+            cell.setLayoutParams(new LinearLayout.LayoutParams(width, height));
             mLayoutPrincipal.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -85,30 +179,29 @@ public class CustomGrid extends RecyclerView.Adapter<CustomGrid.ItemViewHolder> 
                     //Não pode inserir essa opção aqui pois se cancelar o pedido de GPS vai fechar a app!!!
                     //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     v.getContext().startActivity(intent);
-/*
-                    Oferecimento offer = mListaOferecimentos.get((Integer) v.getTag());
 
-                    //Layout customizado na Dialog
-                    Context c = v.getContext();
-                    LayoutInflater l = LayoutInflater.from(c);
-                    final View view = l.inflate(R.layout.layout_dialog, null, false);
-                    //final View view = LayoutInflater.from(c).inflate(R.layout.layout_dialog, null, false);
-                    TextView texto = (TextView) view.findViewById(R.id.edtParametro);
-                    texto.setText(offer.getSigla());
-
-                    Button botao = (Button) view.findViewById(R.id.btnDialog);
-
-                    final Dialog d = new Dialog(v.getContext());
-                    d.setContentView(view);
-                    d.setCancelable(false); //Modal behavior
-                    d.show();
-                    botao.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            d.dismiss();
-                        }
-                    });
-                    */
+//                    Oferecimento offer = mListaOferecimentos.get((Integer) v.getTag());
+//
+//                    //Layout customizado na Dialog
+//                    Context c = v.getContext();
+//                    LayoutInflater l = LayoutInflater.from(c);
+//                    final View view = l.inflate(R.layout.layout_dialog, null, false);
+//                    //final View view = LayoutInflater.from(c).inflate(R.layout.layout_dialog, null, false);
+//                    TextView texto = (TextView) view.findViewById(R.id.edtParametro);
+//                    texto.setText(offer.getSigla());
+//
+//                    Button botao = (Button) view.findViewById(R.id.btnDialog);
+//
+//                    final Dialog d = new Dialog(v.getContext());
+//                    d.setContentView(view);
+//                    d.setCancelable(false); //Modal behavior
+//                    d.show();
+//                    botao.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            d.dismiss();
+//                        }
+//                    });
                 }
             });
         }
--- app/src/main/java/br/edu/ifspsaocarlos/sdm/notificacaoifsp/layout/GridNotificationsFragment.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/notificacaoifsp/layout/GridNotificationsFragment.java
@@ -1,7 +1,10 @@
 package br.edu.ifspsaocarlos.sdm.notificacaoifsp.layout;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.os.Build;
@@ -12,10 +15,19 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -55,6 +67,22 @@ public class GridNotificationsFragment extends TemplateFragment{
         // Required empty public constructor
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {        
+        super.onConfigurationChanged(newConfig);
+        int currentOrientation = getResources().getConfiguration().orientation;
+
+        if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE){
+            Log.v("TAG","Landscape !!!");
+        }
+        else {
+            Log.v("TAG","Portrait !!!");
+        }
+        
+        //// TODO: 6/15/2017 testar e verificar se a transição ficou lenta!!!
+        configurarAdapter();
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -77,6 +105,42 @@ public class GridNotificationsFragment extends TemplateFragment{
     private RecyclerView mRecyclerView;
     private CustomGrid mListaApdapter;
     private View thisView;
+    private LinearLayout week;
+
+    private int configurarLegenda(){
+        //week = (LinearLayout) thisView.findViewById(R.id.gridWeek);
+        week.removeAllViews();
+        String[] days = getResources().getStringArray(R.array.days_of_week);
+
+        LayoutParams paramsExample = new LayoutParams(0, LayoutParams.MATCH_PARENT, 1f);
+        for (int i = 0; i < days.length; i++)
+        {
+            TextView dayName = new TextView(getContext());
+            dayName.setText(days[i]);
+            dayName.setBackgroundResource(R.color.colorPrimary);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                dayName.setTextColor(getResources().getColor(R.color.colorOptionMap, null));
+            }else{
+                dayName.setTextColor(getResources().getColor(R.color.colorOptionMap));
+            }
+            dayName.setLayoutParams(paramsExample);
+            dayName.setGravity(Gravity.CENTER);
+            week.addView(dayName);
+        }
+
+        //int posicao[] = new int[2];
+        //LinearLayout bar = (LinearLayout) thisView.findViewById(R.id.gridWeek);
+        //bar.getLocationOnScreen(posicao);
+
+        Rect rectf = new Rect();
+        Rect rectbar = new Rect();
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        toolbar.getGlobalVisibleRect(rectbar);
+        week.getGlobalVisibleRect(rectf);
+
+        return rectf.bottom;
+    }
+
     private void configurarAdapter() {
         ArrayList<Oferecimento> mListaOferecimentos = new ArrayList<Oferecimento>();
         Date currentDate = Calendar.getInstance().getTime();
@@ -85,7 +149,11 @@ public class GridNotificationsFragment extends TemplateFragment{
 //        Log.d("TCC", output);
         Calendar c = Calendar.getInstance();
         c.setTime(currentDate); // Now use today date.
-        for (char i = 0; i < 102; i++) {
+
+        Point size = new Point();
+        getActivity().getWindowManager().getDefaultDisplay().getSize(size);
+
+        for (char i = 0; i < 25; i++) {
             Oferecimento offer = new Oferecimento();
 
             c.add(Calendar.DATE, i); // Adding days
@@ -114,27 +182,79 @@ public class GridNotificationsFragment extends TemplateFragment{
             mListaOferecimentos.add(offer);
         }
 
+        int altura = configurarLegenda();
+        Log.d("TCC", "Altura barras: " + altura);
+
         if (mListaOferecimentos != null) {
+
             Log.d("TCC", thisView.getClass().getName());
             mRecyclerView = (RecyclerView) thisView.findViewById(R.id.recycler_view);
             mRecyclerView.setHasFixedSize(true);
-            mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
+            //mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
             GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), NUMBER_COLUMN, GridLayoutManager.VERTICAL, false);
+            //merge of colums
+//            gridLayoutManager.setSpanSizeLookup(
+//                    new GridLayoutManager.SpanSizeLookup() {
+//                        @Override
+//                        public int getSpanSize(int position) {
+//                                //Stagger every other row
+//                            return (position == 7 ? 2 : 1);
+//                            //return (position % 2 == 0 ? 3 : 2); //merge de 3 se não merge de 2
+//                        }
+//                    });
             mRecyclerView.setLayoutManager(gridLayoutManager);
 
-            mListaApdapter = new CustomGrid(mListaOferecimentos);
+            mListaApdapter = new CustomGrid(mListaOferecimentos, size, altura);
             mRecyclerView.setAdapter(mListaApdapter);
+
+            /*
+            // Cria o adaptador que preencherá as células da tela com o conteúdo da lista
+            Adapter adaptador = new CustomGrid(this, mListaOferecimentos);
+            setAta(adaptador);*/
+            Log.d("TCC", "After set");
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("TCC", "Before configurarAdapter");
         // Inflate the layout for this fragment
         //thisView = inflater.inflate(R.layout.fragment_grid_notifications, container, false);
         thisView = inflater.inflate(R.layout.fragment_grid_notifications, null);
-        configurarAdapter();
+        week = (LinearLayout) thisView.findViewById(R.id.gridWeek);
+        week.post(new Runnable()
+        {
+            public void run()
+            {
+                Log.d("TCC", "RUNN!!!");
+                configurarAdapter();
+            }
+        });
+        Log.d("TCC", "After onCreateView");
         return thisView;
     }
+/*
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        Log.d("TCC", "onActivityCreated");
+        //configurarAdapter();
+    }
+
+    public void onResume(){
+        super.onResume();
+        Log.d("TCC", "onResume");
+        week = (LinearLayout) thisView.findViewById(R.id.gridWeek);
+        week.post(new Runnable()
+        {
+            public void run()
+            {
+                Log.d("TCC", "RUNN!!!");
+                configurarAdapter();
+            }
+        });
+        Log.d("TCC", "onResume end");
+        //configurarAdapter();
+    }
+    */
 }
